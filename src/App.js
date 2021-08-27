8263db9b4a392f025508535bf6c6bb217804486d
moshbub34 <89669611+moshbub34@users.noreply.github.com>
2021-08-27 19:52:03 -0400

    Test Update
    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Cursor from "./components/Cursor";
 import HomePage from "./components/HomePage";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Nvbar";
 
 function App() {
   return (
